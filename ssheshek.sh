560cbbe762611b3481ce2f32142d7b542f2df865
r01 <contact@r01.li>
2023-12-30 11:54:53 +0100

    fix $1 and add return values

--- ssheshek.sh
+++ ssheshek.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-ReallocatedSectorCt=$(smartctl -a -j $0 | jq '.ata_smart_attributes.table[] | select(.name=="Reallocated_Sector_Ct") | .raw.value')
-Current_Pending_Sector=$(smartctl -a -j $0 | jq '.ata_smart_attributes.table[] | select(.name == "Current_Pending_Sector") | .raw.value')
+
+ReallocatedSectorCt=$(smartctl -a -j $1 | jq '.ata_smart_attributes.table[] | select(.name=="Reallocated_Sector_Ct") | .raw.value')
+
+if [[ "$ReallocatedSectorCt" -ne "0" ]]
+then
+  return 1
+fi
+
+Current_Pending_Sector=$(smartctl -a -j $1 | jq '.ata_smart_attributes.table[] | select(.name == "Current_Pending_Sector") | .raw.value')
+
+if [[ "$Current_Pending_Sector" -ne 0 ]]
+then
+  return 2
+fi
+
+return 0
