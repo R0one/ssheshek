b4ef6cda640e1ef0db78ab713a476483eb578fc7
r01 <contact@r01.li>
2023-12-30 12:28:08 +0100

    fix arithmetic check for subcommand ret

--- ssheshek.sh
+++ ssheshek.sh
@@ -2,7 +2,7 @@
 
 ReallocatedSectorCt=$(smartctl -a -j $1 | jq '.ata_smart_attributes.table[] | select(.name=="Reallocated_Sector_Ct") | .raw.value')
 
-if [ $? -ne 0 ] ; then exit 1 ; fi
+if (( $? > 0 )) ; then exit 1 ; fi
 
 if [[ "$ReallocatedSectorCt" -ne "0" ]]
 then
@@ -11,7 +11,7 @@ fi
 
 Current_Pending_Sector=$(smartctl -a -j $1 | jq '.ata_smart_attributes.table[] | select(.name == "Current_Pending_Sector") | .raw.value')
 
-if [ $? -ne 0 ] ; then exit 1 ; fi
+if (( $? > 0 )) ; then exit 1 ; fi
 
 if [[ "$Current_Pending_Sector" -ne 0 ]]
 then
