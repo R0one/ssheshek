b6acb137b6bd137bfe01db93548941b504c468bd
r01 <contact@r01.li>
2023-12-30 12:07:34 +0100

    fail on smartctl failure

--- ssheshek.sh
+++ ssheshek.sh
@@ -2,16 +2,20 @@
 
 ReallocatedSectorCt=$(smartctl -a -j $1 | jq '.ata_smart_attributes.table[] | select(.name=="Reallocated_Sector_Ct") | .raw.value')
 
+if [ $? -ne 0 ] ; then exit 1 ; fi
+
 if [[ "$ReallocatedSectorCt" -ne "0" ]]
 then
-  exit 1
+  exit 2
 fi
 
 Current_Pending_Sector=$(smartctl -a -j $1 | jq '.ata_smart_attributes.table[] | select(.name == "Current_Pending_Sector") | .raw.value')
 
+if [ $? -ne 0 ] ; then exit 1 ; fi
+
 if [[ "$Current_Pending_Sector" -ne 0 ]]
 then
-  exit 2
+  exit 3
 fi
 
 exit 0
